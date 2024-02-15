d5b3c15cad9dcdbbb37653e42d682269a545faf7
Batista Alvarez Natanael <natanael.batista@upr.edu>
2024-02-15 00:25:19 -0400

    ExpenseChart.js added, render a pie chart with user expenses in different categories with their legends

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 'use client'
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import RenderBarChart from "./barChart";
+import RenderPieChart from "./ExpenseChart"; // Import the RenderPieChart component
 import theNothinger from "./somethingElse";
 import ExpenseCategoryItem from './ExpenseCategoryItem';
 
@@ -8,11 +9,10 @@ export default function Home() {
   const [showBarChart, setShowBarChart] = useState(false);
 
   const toggleChange = () => {
-      setShowBarChart(!showBarChart);
+    setShowBarChart(!showBarChart);
   }
 
   return (
-
     <>
       <main className="container max-w-2x1 px-6 mx-auto">
         <section className="py-3">
@@ -20,23 +20,24 @@ export default function Home() {
           <h2 className="text-4x1 font-bold">$100</h2>
         </section>
 
-          {/* Add Expenses feature */}
-          <section>
-            <h3 className='flex-2x1'>My Expenses</h3>
-            <div className='flex flex-col gap-2 mt-6'>
-                <ExpenseCategoryItem color={'#014'} title={'Mock-cost'} total={300}/>
-                <ExpenseCategoryItem color={'#032'} title={'Mock-cost'} total={125}/>
-                <ExpenseCategoryItem color={'#121'} title={"Mock-cost"} total={200}/>
-            </div>
-          </section>
+        {/* Add Expenses feature */}
+        <section>
+          <h3 className='flex-2x1'>My Expenses</h3>
+          <div className='flex flex-col gap-2 mt-6'>
+            <ExpenseCategoryItem color={'#014'} title={'Mock-cost'} total={300}/>
+            <ExpenseCategoryItem color={'#032'} title={'Mock-cost'} total={125}/>
+            <ExpenseCategoryItem color={'#121'} title={"Mock-cost"} total={200}/>
+          </div>
+        </section>
 
         <div className="mt-2 group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
-          <button onClick={toggleChange}>{showBarChart ? 'Show Nothing' : 'Show Bar Chart'}</button>
+          <button onClick={toggleChange}>{showBarChart ? 'Show Pie Chart' : 'Show Bar Chart'}</button>
         </div>
-        <section className='max-w-2x1 px-6 mx-auto'>{showBarChart ? <RenderBarChart /> : <theNothinger />}</section>
         
+        <section className='max-w-2x1 px-6 mx-auto'>
+          {showBarChart ? <RenderBarChart /> : <RenderPieChart />}
+        </section>
       </main>
     </>
-    
   );
 }
