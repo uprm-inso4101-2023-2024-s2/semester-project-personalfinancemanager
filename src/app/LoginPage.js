239073912215d84b74d9c27cfc30b01a10680e3b
Goya01 <steven.lopez11@upr.edu>
2024-02-20 18:16:29 -0400

    Added 2 New buttons, Forgot password and Create a new account

--- src/app/LoginPage.js
+++ src/app/LoginPage.js
@@ -15,7 +15,7 @@ function LoginPage({ onLogin }) {
     };
 
     return (
-        <div className="flex flex-col items-center justify-centerh-screen">
+        <div className="flex flex-col items-center h-screen">
             <h2 className="text-2xl font-semibold mb-3">Login Page</h2>
             <div className="flex flex-col gap-4">
                 <div>
@@ -28,9 +28,13 @@ function LoginPage({ onLogin }) {
                         <input type={showPassword ? 'text' : 'password'} value={password} onChange={(e) => setPassword(e.target.value)} className="border px-3 py-1 rounded text-black" />
                         <button className="absolute top-1/2 right-2 transform -translate-y-1/2 text-black" onClick={toggleShowPassword}>{showPassword ? 'Hide' : 'Show'}</button>
                     </div>
+                    <button className="text-white underline mt-2" onClick={() => console.log("Forgot Password?")}>Forgot Password?</button>
                 </div>
+                <button onClick={handleLogin} className="bg-green-500 text-white px-4 py-2 rounded mt-3">Login</button>
             </div>
-            <button onClick={handleLogin} className="bg-green-500 text-white px-4 py-2 rounded mt-3">Login</button>
+            <div className="mt-3 text-white">
+                No account? <button className="underline" onClick={() => console.log("Create an account")}>Create One</button>
+            </div>
         </div>
     );
 }
