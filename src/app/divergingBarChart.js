635af4a490c8fd49610452f02c00c459fc54e0c7
fernandopizarro1 <fernando.pizarro1@upr.edu>
2024-02-29 22:13:39 -0400

    Removed UseState

--- src/app/divergingBarChart.js
+++ src/app/divergingBarChart.js
@@ -1,5 +1,5 @@
 'use client'
-import React, {useState, useRef, useEffect} from 'react';
+import React, {useRef, useEffect} from 'react';
 import * as d3 from 'd3';
 
 export default function RenderDBC( {
