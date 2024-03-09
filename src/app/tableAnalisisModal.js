3e408a8f59a350d35bdfb63024092f82849bc0e8
Fernan2908 <fernando.sierra3@upr.edu>
2024-03-09 18:31:29 -0400

    Add income table to TableAnalisisModal component

--- src/app/tableAnalisisModal.js
+++ src/app/tableAnalisisModal.js
@@ -9,9 +9,39 @@ function TableAnalisisModal({ show, onClose }) {
                     <span className="text-2xl">&times;</span>
                 </button>
             </div>
-            <div className="flex justify-center items-center">
-                <p>Table Analisis</p>
+
+            <div className="flex justify-center items-center mb-6">
+                <p className="text-xl" >Income</p>
             </div>
+            
+            <table className="flex justify-center">
+                <tbody>
+                    <tr>
+                        <th>Income</th>
+                        <th>Median</th>
+                        <th>Mean</th>
+                        <th>Average</th>
+                    </tr>
+                    <tr>
+                        <td>Date</td>
+                        <td>Row 1, Cell 2</td>
+                        <td>Row 1, Cell 3</td>
+                        <td>Row 1, Cell 4</td>
+                    </tr>
+                    <tr>
+                        <td>Month</td>
+                        <td>Row 2, Cell 2</td>
+                        <td>Row 2, Cell 3</td>
+                        <td>Row 2, Cell 4</td>
+                    </tr>
+                    <tr>
+                        <td>Week</td>
+                        <td>Row 3, Cell 2</td>
+                        <td>Row 3, Cell 3</td>
+                        <td>Row 3, Cell 4</td>
+                    </tr>
+                </tbody>
+            </table>
         </Modal>
     );
 }
