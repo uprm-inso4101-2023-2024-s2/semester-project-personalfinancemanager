c35cf7f24ad6c5b9e064a96da03ac01eb9b1ab04
yadrielsamir <yadrielsamir@gmail.com>
2024-04-15 18:55:13 -0400

    changed color to red

--- src/app/Modals/modal.js
+++ src/app/Modals/modal.js
@@ -11,7 +11,7 @@ function Modal({show, onClose, children}){
           onClick={() => {
             onClose(false);
           }}
-        className='w-10 h-10 mb-4 font-bold rounded-full bg-slate-500'
+        className='w-10 h-10 mb-4 font-bold rounded-full bg-red-500'
         >
           X
         </button>
